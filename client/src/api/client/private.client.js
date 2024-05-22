6cdd70af3ec6ea010f0a1ac45b505b0374b111ac
alxnblt <alikhanbulat2605@gmail.com>
2024-05-23 00:46:43 +0500

    update api

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "red-flix-api.vercel.app/api/v1/";
+const baseURL = "https://red-flix-api.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
