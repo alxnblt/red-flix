e98399746ed3a978d960aacd568230a9f899b27e
alxnblt <alikhanbulat2605@gmail.com>
2024-05-24 15:49:10 +0500

    up

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://red-flix-api.vercel.app/api/v1/";
+// const baseURL = "https://red-flix-api.vercel.app/api/v1/";
+const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+
 
 const privateClient = axios.create({
   baseURL,
